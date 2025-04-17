a5e4598e3cbe3520cd0c65f6bb80abce05543ba9
rebec818 <ana.salguero3883@utc.edu.ec>
2025-04-17 08:00:26 -0500

    h
    m

--- Sistema_Farmacia/Sistema_Farmacia/settings.py
+++ Sistema_Farmacia/Sistema_Farmacia/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
     'Farmacia',
+    'import_export',
 ]
 
 MIDDLEWARE = [
